86a589bcb3ca786892df0f910ca595e03411eedb
Emerson Bottero <emersonbottero@gmail.com>
2022-01-18 19:40:06 -0300

    who am I

--- ServiceNow.Core/deploy.sh
+++ ServiceNow.Core/deploy.sh
@@ -9,6 +9,10 @@ npm run docs:build
 # navigate into the build output directory
 cd docs/.vitepress/dist
 
+# who am I?
+git config --global user.email "emersonbottero@gmail.com"
+git config --global user.name "Emerson Bottero Branco"
+
 git init
 git add -A
 git commit -m 'deploy'
